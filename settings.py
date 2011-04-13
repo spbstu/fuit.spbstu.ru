edaa05ecb1a5d9c94d93c182c271053267ff7527
Victoria Shishkina <vita@avalon.ru>
2011-04-13 16:56:40 +0400

    settings modified

--- settings.py
+++ settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'fuit.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
