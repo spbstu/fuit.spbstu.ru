a9df8f58bd404aa5db22d3bc4f809c8407331b36
vita <vita@avalon.ru>
2011-09-28 11:00:49 +0400

    Django 1.3

--- urls.py
+++ urls.py
@@ -1,39 +1,17 @@
-# -*- coding: utf-8 -*-
-import os
-from django.conf.urls.defaults import *
-from django.conf import settings
-from www.views import *
-
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
-
-#t = os.path.join(settings.MEDIA_ROOT, "css")
-#print t
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    (r'', include('fuit.www.urls')),
- 
-    (r'^css/(?P<path>.*)$', 'django.views.static.serve',
-    {'document_root': os.path.join(settings.MEDIA_ROOT, "css")} ),
-    (r'^i/(?P<path>.*)$', 'django.views.static.serve',
-    {'document_root': os.path.join(settings.MEDIA_ROOT, "i")} ),
-    (r'^img/(?P<path>.*)$', 'django.views.static.serve',
-    {'document_root': os.path.join(settings.MEDIA_ROOT, "img")} ),
-
-    (r'^js/(?P<path>.*)$', 'django.views.static.serve',
-    {'document_root': os.path.join(settings.MEDIA_ROOT, "js")} ),
-    (r'^media/js/(?P<path>.*)$', 'django.views.static.serve',
-    {'document_root': os.path.join(settings.MEDIA_ROOT, "js")} ),
+    # Examples:
+    # url(r'^$', 'fuit2.views.home', name='home'),
+    # url(r'^fuit2/', include('fuit2.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/filebrowser/', include('filebrowser.urls')),
-    
-    
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-)   
+    # url(r'^admin/', include(admin.site.urls)),
+)
