0514b46f281122bb755a3cc5512a60d983da6e5e
Mikhail Baranov <mikhail.baranov@gmail.com>
2011-10-25 17:44:05 +0400

    Исправления шаблонов
    Новости - проверка типа, испрален вывод содержимого
    Вывод названия страницы как заголовка
    Проверка ключа show_sidebar

--- pages/models.py
+++ pages/models.py
@@ -5,6 +5,7 @@ from django.contrib.flatpages.models import FlatPage
 # Create your models here.
 class Page(FlatPage):
     show_in_menu = models.BooleanField(default = False)
+    show_sidebar = models.BooleanField(default = False)
     class Meta:
         verbose_name = u'Страница'
         verbose_name_plural = u'Страницы'
