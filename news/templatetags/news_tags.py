a6bdc7277802515f234e4b6beccb57f387a25cb8
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-04-04 15:36:10 +0400

    Предотвращение вывода пустого списка новостей деканата
    Проверка на то, что запрос всех непротухших новостей возвращает хоть
    что-то

--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -1,20 +1,26 @@
-import datetime, re
+import datetime
+import re
 from django import template
 from news.models import GlobalNews, DeansNews
 from django.db.models import Max, Min
 
 register = template.Library()
 
+
 @register.inclusion_tag('list.html')
 def news_list():
-    news = GlobalNews.objects.filter(show = True)[:5]
+    news = GlobalNews.objects.filter(show=True)[:5]
     return {'news': news, 'show_date': 1}
+
 
 @register.inclusion_tag('list.html')
 def deans_news_list():
-    news = DeansNews.objects.filter(show = True, expired__gt =
-            datetime.date.today())
+    news = DeansNews.objects.filter(show=True,
+        expired__gt=datetime.date.today())
+    if (len(news) == 0):
+        news = DeansNews.objects.filter(show=True)[:2]
     return {'news': news}
+
 
 @register.inclusion_tag('pager.html', takes_context=True)
 def get_pager_by_years(context, scope):
@@ -23,4 +29,3 @@ def get_pager_by_years(context, scope):
     years = [{'title': year, 'url': '%s%s/' % (basepath, year)} for year in
         range(newsObj.objects.aggregate(Min('date'))['date__min'].year, datetime.date.today().year + 1)]
     return {'years': years, 'request': context['request']}
-
