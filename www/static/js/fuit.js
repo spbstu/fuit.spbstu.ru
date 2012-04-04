d0ebae218cee1ff1c0ca35c38d48074a467ee84d
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-04-04 16:34:40 +0400

    Заменил все анонимные функции на именованые

--- www/static/js/fuit.js
+++ www/static/js/fuit.js
@@ -13,23 +13,13 @@ var today = new Date();
 })();
 
 /*
- * Установка кастомного хеша 
- * и прокрутка страницы к верху
- */
-
-if (window.location.hash) {
-	window.location.hash = '!/' + window.location.hash.replace(/#(!\/)*/, '');
-	frame.animate({scrollTop: 0});
-}
-
-/*
  * Функция отображения вкладок
  * Принимает в себя ID целевой вкладки
  * который в дальнейшем очищается от дополнительных символов
  */
 
-var showTab = function(targetTab) {
-	if (targetTab === "" || isNull(targetTab)) {
+function showTab(targetTab) {
+	if (targetTab === "" || targetTab === null) {
 		return true;
 	}
 	target = targetTab.replace(/#(!\/)*/, '');
@@ -45,11 +35,87 @@ var showTab = function(targetTab) {
 	$('#' + target)
 		.addClass('tab-content-visible');
 	return false;
-};
+}
 
+function carousel(el, target) {
+	var content = el.find('.carousel-content');
+	var step = content.find('.carousel-item').width();
+	var count = content.find('.carousel-item').length;
+	var pager = $('.promo-pager li a');
+	if (Math.abs(parseInt(content.css('left'))) < step * (count-1)) {
+		item++;
+	} else {
+		item = 0;
+	}
+	if (target) {
+		item = target;
+		clearInterval(timer);
+		timer = setInterval(function() {carousel($("#promo"), false);}, carouselInterval);
+	}
+	content.animate({left: '-'+step*item}, carouselDuration);
+	pager.removeClass('current');
+	pager.eq(item).addClass('current');
+}
+
+	
+/*
+ * Добавление дней недели в расписание
+ * Определяем дату на основании значения в таблице
+ * Определяем день недели и добавляем к имеющемуся содержимому
+ * 
+ * Если событие прошло - добавляем экстра-класс
+ */
+
+function scheduleSetExtra() {
+	var dateArr = $(this).text().split('.');
+	var dt = new Date(dateArr[2],dateArr[1] - 1,dateArr[0]);
+	console.log(dt);
+	var weekDay = weekDays[dt.getDay()];
+	$(this).html($(this).text() + '<br /><span class="comment">' + weekDay + '</span>');
+	
+	if(today > dt) {
+		console.log($(this).closest('tr'));
+		$(this).closest('tr').addClass('past');
+	}
+}
+
+/*
+ * Функция переключения видимости элементов
+ * принимает в себя объект event, откуда дёргает event.data
+ * event.data {
+ * 	el: $(obj) - ссылка на целевой объект
+ * 	showMsg: string - текст, отображаемая у ссылки после скрытия цели
+ * 	hideMsg: string - текст, отображаемый у ссылки при видимой цели
+ * }
+ * */
+
+function toggleEl(event) {
+		var objData = event.data;
+		var obj = $(event.target);
+		var el = $(objData.el);
+		if (el.is(':hidden')) {
+			el.show();
+			obj.text(objData.hideMsg);
+		} else {
+			el.hide();
+			obj.text(objData.showMsg);
+		}
+		return false;
+	}
 
 function init() {
 	frame = ( $.browser.safari ) ? $( 'body' ) : $( 'html' );
+
+/*
+ * Установка кастомного хеша 
+ * и прокрутка страницы к верху
+ */
+
+	if (window.location.hash) {
+		window.location.hash = '!/' + window.location.hash.replace(/#(!\/)*/, '');
+		frame.animate({scrollTop: 0});
+	}
+	
 	$('body').click(function() {
 		$('#core-contacts .contact-details')
 			.removeClass('contact-details-show');
@@ -75,54 +141,13 @@ function init() {
 		return false;
 	});
 	
-	var carousel = function(el, target) {
-		var content = el.find('.carousel-content');
-		var step = content.find('.carousel-item').width();
-		var count = content.find('.carousel-item').length;
-		var pager = $('.promo-pager li a');
-		if (Math.abs(parseInt(content.css('left'))) < step * (count-1)) {
-			item++;
-		} else {
-			item = 0;
-		}
-		if (target) {
-			item = target;
-			clearInterval(timer);
-			timer = setInterval(function() {carousel($("#promo"), false);}, carouselInterval);
-		}
-		content.animate({left: '-'+step*item}, carouselDuration);
-		pager.removeClass('current');
-		pager.eq(item).addClass('current');
-	};
 	$('.promo-pager a').click(function() {
 		carousel($("#promo"), $(this).parent().index());
 		return false;
 	});
-	timer = setInterval(function() {carousel($("#promo"), false);}, carouselInterval);
-	
-/*
- * Функция переключения видимости элементов
- * Принимает в себя объект event, откуда дёргает event.data
- * event.data {
- * 	el: $(obj) - ссылка на целевой объект
- * 	showMsg: string - текст, отображаемая у ссылки после скрытия цели
- * 	hideMsg: string - текст, отображаемый у ссылки при видимой цели
- * }
- * */
-
-	var toggleEl = function(event) {
-		var objData = event.data;
-		var obj = $(event.target);
-		var el = $(objData.el);
-		if (el.is(':hidden')) {
-			el.show();
-			obj.text(objData.hideMsg);
-		} else {
-			el.hide();
-			obj.text(objData.showMsg);
-		}
-		return false;
-	};
+	timer = setInterval(function() {
+		carousel($("#promo"), false);
+	}, carouselInterval);
 	
 /*
  * Создаём объект параметров переключателя "консультации"
@@ -150,27 +175,7 @@ function init() {
 		var targetTab = $(this).find('a').attr('href');
 		return showTab(targetTab);
 	});
-	
-/*
- * Добавление дней недели в расписание
- * Определяем дату на основании значения в таблице
- * Определяем день недели и добавляем к имеющемуся содержимому
- * 
- * Если событие прошло - добавляем экстра-класс
- */
 
-	var scheduleSetExtra = function() {
-		var dateArr = $(this).text().split('.');
-		var dt = new Date(dateArr[2],dateArr[1] - 1,dateArr[0]);
-		console.log(dt);
-		var weekDay = weekDays[dt.getDay()];
-		$(this).html($(this).text() + '<br /><span class="comment">' + weekDay + '</span>');
-		
-		if(today > dt) {
-			console.log($(this).closest('tr'));
-			$(this).closest('tr').addClass('past');
-		}
-	};
 	
 	var day = $('.schedule.session .time');
 	
