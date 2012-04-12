7d68c79a96837628d93c1c5d144796163b7548b5
Mikhail Baranov <mikhailb@avalon.ru>
2012-04-12 17:55:01 +0400

    Fixed iports

--- static/js/fuit.ie.js
+++
@@ -1,5 +0,0 @@
-var a = ['section', 'article', 'nav', 'header', 'footer', 'aside', 'details', 'figcaption', 'figure', 'hgroup', 'menu'];
-
-for (var i = 0, j = a.length; i < j; i++) {
-    document.createElement(a[i]);
-}
