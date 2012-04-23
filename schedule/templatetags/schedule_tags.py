c52343fc93b84c0413cc3f1beac242300378d81a
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-04-23 15:04:40 +0400

    Исправление опечаток

--- schedule/templatetags/schedule_tags.py
+++ schedule/templatetags/schedule_tags.py
@@ -1,13 +1,14 @@
 # -*- coding: utf8 -*-
 
 from django import template
-from schedule.models import Subjects, Groups
+from schedule.models import Groups
 
 register = template.Library()
 
-departments = [r'Компьютерные информационных технологии в проектировании',
+departments = [r'Компьютерные информационные технологии в проектировании',
                r'Управление в социально-экономических системах',
                r'Национальная безопасность']
+
 
 @register.inclusion_tag('groups_list.html')
 def groups_list(current_group=False):
@@ -15,9 +16,9 @@ def groups_list(current_group=False):
     for id, department in enumerate(departments):
         x = []
         groups.append((department, x))
-        for year in xrange(1,6):
+        for year in xrange(1, 6):
             groups_by_year = {}
-            for group in Groups.objects.filter(number__regex='%s..%s/.' % (year, id+1)):
+            for group in Groups.objects.filter(number__regex='%s..%s/.' % (year, id + 1)):
                 if current_group:
                     group.css_class = 'current'
                 groups_by_year[group.number.replace('/', '-')] = group
@@ -28,7 +29,8 @@ def groups_list(current_group=False):
         level = 0
     return {'groups': groups, 'level': level, 'departments': departments}
 
+
 @register.filter
 def weeks_repeat(value):
-    weeks = [u'1, 2',u'1',u'2']
+    weeks = [u'1, 2', u'1', u'2']
     return weeks[value]
