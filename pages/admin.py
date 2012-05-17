fbe806b4136e437f5604dee51423dc29db574817
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-05-17 16:53:50 +0400

    Ссылка на редактирование страницы и возврат обратно на саму страницу

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.flatpages.models import FlatPage
+from django.http import HttpResponseRedirect
 
 from pages.models import *
 from fuitadmin import ExtAdmin
@@ -8,6 +9,13 @@ from fuitadmin import ExtAdmin
 class PagesAdmin(ExtAdmin):
     list_display = ('title', 'url', 'show_in_menu', 'show_sidebar')
     list_display_links = ('title',)
+
+    def response_change(self, request, obj):
+        if "back" in request.GET:
+            return HttpResponseRedirect(request.GET["back"])
+        else:
+            return ExtAdmin.response_change(self, request, obj)
+
 
 admin.site.register(Page, PagesAdmin)
 admin.site.register(Attachment)
